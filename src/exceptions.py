eb8a6e32a9d435ecf166398a5b8cf1d6eed091bc
Lauri Ajanki <lauri.ajanki@gmail.com>
2024-03-31 16:28:59 +0300

    chore: fix typo

--- src/exceptions.py
+++ src/exceptions.py
@@ -3,5 +3,5 @@ from urllib.parse import quote
 class NotValidArticleException(Exception):
 
 	def __init__(self, title):
-		message = f"https://en.wikipedia.org/wiki/{quote(title)} doesn't apper to be a valid movie article."
+		message = f"https://en.wikipedia.org/wiki/{quote(title)} doesn't appear to be a valid movie article."
 		super(NotValidArticleException, self).__init__(message)
